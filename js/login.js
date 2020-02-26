302a65724de3c95a373ca48d8af9526933294f34
leosoares96 <leo.soares64@outlook.com>
2020-02-26 00:55:49 -0300

    inserção do vue js

--- js/login.js
+++ js/login.js
@@ -1,4 +1,51 @@
 
+new Vue({
+    el: '#app-login',
+    template: `
+        <div class="row">
+            <div class="card col m4 offset-m4 s12">
+            <h3 class="flow-text teal-text center-align">TELA DE LOGIN</h3>
+            <form action="#" class="col s12">
+                <div class="row">
+                    <div class="input-field col s12">
+                        <input v-model="matricula" id="matricula" type="text" class="validate">
+                        <label for="matricula">Matrícula</label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s12">
+                        <input v-model="senha" id="senha" type="password" class="validate">
+                        <label for="senha">Senha</label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s7 right-align">
+                        <a href="#modal1" class="waves-effect waves-light modal-trigger">Ainda não tem acesso? Clique aqui!</a>
+                    </div>
+                    <div class="input-field col s5">
+                        <button v-on:click="logar" type="submit" class="btn waves-effect waves-light right" id="btn-login" name="entrar">Entrar
+                            <i class="material-icons right">send</i>
+                        </button>
+                    </div>
+                </div>
+            </form>
+            </div> 
+        </div>                      
+    `,
+    data(){
+        return {
+            matricula: '',
+            senha: '',
+        }
+    },
+    methods: {
+        logar(){
+            alert(this.matricula+"-"+this.senha);
+        }
+    }
+})
+
+/* SWEET ALERT QUANDO LOGAR */
 $("#btn-login").click(function(){
 
     Swal.fire(
@@ -6,4 +53,14 @@ $("#btn-login").click(function(){
         'LOGIN EFETUADO COM SUCESSO!',
         'success'
     )
+});
+
+/* INSTANCIA O MODAL */
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, options);
+  });
+
+$(document).ready(function(){
+    $('.modal').modal();
 });
